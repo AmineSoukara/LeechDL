32ad49e39594d78e1f8d20bcd0845cf929b59158
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-09-24 16:44:18 +0100

    Update status_message_fn.py

--- tobrot/plugins/status_message_fn.py
+++ tobrot/plugins/status_message_fn.py
@@ -89,10 +89,10 @@ async def status_message_f(client, message):
     used = humanbytes(used)
     free = humanbytes(free)
 
-    ms_g = f"<b>Bot Uptime</b>: <code>{currentTime}</code>\n" \
-        f"<b>Total disk space</b>: <code>{total}</code>\n" \
-        f"<b>Used</b>: <code>{used}</code>\n" \
-        f"<b>Free</b>: <code>{free}</code>\n"
+    ms_g = f"<b>● Bot Uptime</b>: <code>{currentTime}</code>\n" \
+        f"<b>● Total disk space</b>: <code>{total}</code>\n" \
+        f"<b>● Used</b>: <code>{used}</code>\n" \
+        f"<b>● Free</b>: <code>{free}</code>\n"
 
     msg = ms_g + "\n" + msg
     await message.reply_text(msg, quote=True)
